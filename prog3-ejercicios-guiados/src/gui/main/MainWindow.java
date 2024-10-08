6036dfa5db17c2da64276bb2c2b69c32957d4504
Unai Aguilera <unai.aguilera@deusto.es>
2024-10-08 10:49:05 +0200

    Ejercicio GUI.1

---
+++ prog3-ejercicios-guiados/src/gui/main/MainWindow.java
@@ -0,0 +1,22 @@
+package gui.main;
+
+import javax.swing.JFrame;
+
+/**
+ * Ventana principal de la aplicación.
+ */
+public class MainWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MainWindow() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // comportamiento al cerrar
+		setTitle("JJ.OO. Paris 2024"); // título de la ventana
+		setSize(640, 480); // tamaño inicial
+		setLocationRelativeTo(null); // situa la ventana en el centro de la pantalla
+ 		setVisible(true); // hacemos visible la ventana
+	}
+}
---
+++ prog3-ejercicios-guiados/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import gui.main.MainWindow;
+
+/**
+ * Programa principal de la aplicación.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// el programa únicamenente instancia la ventana principal.
+		new MainWindow();
+	}
+
+}
